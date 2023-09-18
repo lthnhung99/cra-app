419930a52bfc645435c230a9b6205bee1d54959b
Hồng Nhung <lenhung14101999@gmail.com>
2023-09-18 14:08:28 +0700

    create student

--- src/components/Content.jsx
+++ src/components/Content.jsx
@@ -1,9 +1,50 @@
 import React, { useState, useEffect } from "react";
+import { useForm } from 'react-hook-form';
+import { yupResolver } from "@hookform/resolvers/yup"
+import * as yup from "yup"
+const registerSchema = yup.object({
+    name: yup.string()
+        .required("Name là bắt buộc")
+        .min(5, "Tên phải từ 5 ký tự!"),
+    city: yup.string()
+        .required("City là bắt buộc"),
+    gender: yup.string().required(),
+    mark: yup.number()
+        .required()
+        .min(0, "Mark phải lớn hơn hoặc 0")
+        .max(10, "Mark phải nhỏ hơn hoặc bằng 10")
+        .typeError("Không hợp lệ"),
+    age: yup.number()
+        .required()
+        .positive()
+        .max(40)
+        .typeError("Không hợp lệ")
+})
 const Content = () => {
     const [studentList, setStudentList] = useState([])
     const [currentPage, setCurrentPage] = useState(1)
     const [totalPage, setTotalPage] = useState(0)
     const [action, setAction] = useState('next')
+    const { register, handleSubmit, formState: { errors }, reset } = useForm({
+        resolver: yupResolver(registerSchema)
+    })
+
+    const handleCreate = (data) => {
+        console.log(data);
+        postData("https://js-post-api.herokuapp.com/api/students", data)
+    }
+    async function postData(url, data) {
+        // Default options are marked with *
+        const response = await fetch(url, {
+            method: "POST",
+            headers: {
+                "Content-Type": "application/json",
+            },
+            body: JSON.stringify(data),
+        });
+        return response.json();
+    }
+
 
     useEffect(() => {
         async function getPost() {
@@ -44,6 +85,7 @@ const Content = () => {
     //     })
 
     // }
+
     return (
         <div className="container-fluid">
             <h1>List Student</h1>
@@ -108,6 +150,48 @@ const Content = () => {
                     </li>
                 </ul>
             </nav>
+
+            <div className="container d-flex justify-content-center" style={{ backgroundColor: "lightblue" }}>
+                <div className="row col-md-6">
+                    <h1 className="text-center mt-5">Form create</h1>
+                    <form onSubmit={handleSubmit(handleCreate)}>
+                        <div className="form-group mb-3">
+                            <label className="lable-form">Name</label>
+                            <input type="text" className="form-control" {...register('name')} />
+                            <span className="text-danger">{errors?.name?.message}</span>
+                        </div>
+                        <div className="form-group mb-3">
+                            <label className="lable-form">Gender</label>
+                            <select className="form-select" {...register('gender')}>
+
+                                <option value="Male">Male</option>
+                                <option value="Female">Female</option>
+                            </select>
+                            {/* <input type="text" className="form-control" {...register('gender')} /> */}
+
+                        </div>
+                        <div className="form-group mb-3">
+                            <label className="lable-form">Age</label>
+                            <input type="number" className="form-control" {...register('age')} />
+                            <span className="text-danger">{errors?.age?.message}</span>
+                        </div>
+                        <div className="form-group mb-3">
+                            <label className="lable-form">City</label>
+                            <input type="text" className="form-control" {...register('city')} />
+                            <span className="text-danger">{errors?.city?.message}</span>
+                        </div>
+                        <div className="form-group mb-3">
+                            <label className="lable-form">Mark</label>
+                            <input type="number" className="form-control" {...register('mark')} />
+                            <span className="text-danger">{errors?.mark?.message}</span>
+                        </div>
+                        <div className="form-group mb-3">
+                            <button type="submit" className="btn btn-primary me-3">Save</button>
+                            <button type="button" className="btn btn-danger" onClick={() => reset()}>Cancel</button>
+                        </div>
+                    </form>
+                </div>
+            </div>
 
         </div >
     )
